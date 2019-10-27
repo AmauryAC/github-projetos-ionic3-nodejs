156db592156031f618789bdd3335bdeac68c9a00
Amaury AC <amaury.costa@hotmail.com>
2019-10-27 19:33:38 -0300

    Configurando arquivos iniciais do projeto NoFood

---
+++ PrimeiroProjeto/meuprimeiroprojeto.js
@@ -0,0 +1,10 @@
+var pessoa = {};
+
+pessoa.nome = "Aluno";
+pessoa.sobrenome = "Udemy";
+
+function imprimirPessoa() {
+    return `${pessoa.nome} - ${pessoa.sobrenome}.`;
+}
+
+console.log(imprimirPessoa());

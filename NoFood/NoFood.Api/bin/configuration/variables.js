5239912f0800d30af43a7df8257405bd309b3c1a
Amaury AC <amaury.costa@hotmail.com>
2019-11-03 20:34:22 -0300

    Configurando a conexão com o banco de dados MongoDB, utilizando Mongoose e MongoDB Atlas - Projeto NoFood

--- NoFood/NoFood.Api/bin/configuration/variables.js
+++ NoFood/NoFood.Api/bin/configuration/variables.js
@@ -1,6 +1,9 @@
 const variables = {
     Api: {
         port: process.env.port || 3000
+    },
+    Database: {
+        connection: process.env.connection || 'mongodb+srv://nofoodadmin:admin123@cluster0-hserh.mongodb.net/test?retryWrites=true&w=majority'
     }
 };
 
